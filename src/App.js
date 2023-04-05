12f0d7db0fb6fd51a291832804057edff2a1c22d
Shivang <Shivangkh26@gmail.com>
2023-04-06 00:49:04 +0530

    bootstrap dependency added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corrupti autem iusto, quos sequi voluptatibus dolore asperiores alias accusantium. Adipisci consequatur inventore quod aliquam dolore similique, recusandae facere soluta natus eaque modi cupiditate, at iusto, perspiciatis harum porro repudiandae non! Mollitia laudantium dolore distinctio maiores repellat dolorem aliquam dolor eaque iste officiis harum nostrum dicta rerum iusto unde repudiandae, esse ex obcaecati at cupiditate recusandae non autem eligendi vel. Voluptas consequatur nemo, numquam tempore repellendus laboriosam eum? Molestias consequuntur recusandae at illum architecto magnam molestiae facilis quaerat dicta dolores. Commodi iure, vel deserunt adipisci quo itaque velit animi optio recusandae!
       </header>
     </div>
   );
