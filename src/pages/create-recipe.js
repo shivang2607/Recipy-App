4e232c1699868a01f9cf8f753548dc6ed0aab13a
Shivang <Shivangkh26@gmail.com>
2023-04-06 00:38:38 +0530

    second commit

---
+++ src/pages/create-recipe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateRecipe = () => {
+  return (
+    <div>R</div>
+  )
+}
+
+export default CreateRecipe
---
+++ src/pages/saved-recipes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SavedRecipes = () => {
+  return (
+    <div>savedRecipes</div>
+  )
+}
+
+export default SavedRecipes
